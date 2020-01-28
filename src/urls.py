7b9bdcbc5481a71a16da848bc267d070741e811c
richdu94 <rich.iemmont@gmail.com>
2020-01-29 00:00:55 +0100

    create app for data viz + notebook

--- src/urls.py
+++ src/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls import include
+from django.urls import path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('data-exploration/', include('data_exploration.urls'))
 ]
